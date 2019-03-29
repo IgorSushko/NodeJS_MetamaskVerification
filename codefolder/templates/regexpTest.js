b530dd2efcdf4d6effdc77ce076d9fb57a5471a3
Sushko Igor <igor.sm84@gmail.com>
2019-03-29 14:36:06 +0200

    something with try and catch

--- codefolder/templates/regexpTest.js
+++ codefolder/templates/regexpTest.js
@@ -20,36 +20,47 @@ function wait(milleseconds) {
 
 async function payEther() {
   document.getElementById('transactioninprogress').style.visibility = 'visible';
-  const datas = document.getElementById('hiddenadress').textContent;
-  console.log('adress from page', datas);
-  //const receiver = '0x250ce03d2f095fe3482fb237a23e172af08fbf5c';
-  const sender = currentUser.account;
-  const waitforhash = web3.eth.sendTransaction(
-    {
-      to: datas,
-      from: sender,
-      value: web3.toWei('0.025', 'ether')
-    }, function (err, hash) {
-      console.log('after transaction: ', hash);
-      makehashglobal(hash);
+  try {
+    const datas = document.getElementById('hiddenadress').textContent;
+    console.log('adress from page', datas);
+    //const receiver = '0x250ce03d2f095fe3482fb237a23e172af08fbf5c';
+    const sender = currentUser.account;
+    const waitforhash = web3.eth.sendTransaction(
+      {
+        to: datas,
+        from: sender,
+        value: web3.toWei('0.025', 'ether')
+      }, function (err, hash) {
+        if (err.message.includes("User denied transaction signature")) {
+          console.log('submit was corrupted: ', err);
+          document.getElementById('transactioninprogress').style.visibility = 'hidden';
+          throw new Error('Fatal error!!!')
+        }
+        console.log('after transaction: ', hash);
+        makehashglobal(hash);
+      }
+    );
+    while (globalhash === undefined) {
+      // we are going to check every second if transation is mined or not, once it is mined we'll leave the loop
+      console.log('from the loop: ', waitforhash);
+      await wait(1000);
     }
-  );
-  while (globalhash === undefined) {
-    // we are going to check every second if transation is mined or not, once it is mined we'll leave the loop
-    console.log('from the loop: ', waitforhash);
-    await wait(1000);
-  }
-  console.log('globalhash: ', globalhash);
+    console.log('globalhash: ', globalhash);
 
-  let receipt = null;
-  console.log('receipt inside');
-  while (receipt === null) {
+    let receipt = null;
+    console.log('receipt inside');
+    while (receipt === null) {
     // we are going to check every second if transation is mined or not, once it is mined we'll leave the loop
-    receipt = await getTransactionReceiptPromise(globalhash);
-    console.log('receipt inside loop');
-    await wait(1000);
+      receipt = await getTransactionReceiptPromise(globalhash);
+      console.log('receipt inside loop');
+      await wait(1000);
+    }
+    document.getElementById('transactionresult').style.visibility = 'visible';
+    document.getElementById('transactioninprogress').style.visibility = 'hidden';
+    console.log('Receipt: ', receipt);
+  } catch (err) {
+    console.log('Try was corrupted: ', err);
+    return;
   }
-  document.getElementById('transactionresult').style.visibility = 'visible';
-  document.getElementById('transactioninprogress').style.visibility = 'hidden';
-  console.log('Receipt: ', receipt);
 }
+//https://medium.com/@dan_43404/handling-metamask-rejections-532a4a41caf
